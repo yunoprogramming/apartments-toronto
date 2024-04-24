f665006e951623618e09f2f02a5a7ced5907e32b
yunoprogramming <matthewhosangrose@gmail.com>
2024-04-24 18:04:20 -0400

    newsletter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
 							/>
 							<button
 								type='submit'
-								className='bg-red-600 hover:bg-red-700 text-white font-lovelo font-bold py-2 px-6 rounded-lg shadow-xl'>
+								className='bg-red-600 hover:bg-red-700 text-white font-lovelo font-bold py-3 px-8 rounded-lg shadow-xl'>
 								JOIN NOW
 							</button>
 						</div>
